ef2b995e33985f0e4e80902b0d28d04cc96a3824
Andrej <anweit00@hs-esslingen.de>
2021-11-30 16:37:55 +0100

    20 - full copy for profile-reducer

--- src/index.js
+++ src/index.js
@@ -8,26 +8,16 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
-let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
-                    <App/>
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-
-
-}
-rerenderEntireTree();
-//store.getState()
-store.subscribe(() => {
-
-    rerenderEntireTree();
-});
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 
 // If you want to start measuring performance in your app, pass a function
