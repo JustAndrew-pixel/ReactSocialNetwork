8f059aaccf5b03133e882316175c857faf1e18f5
Andrej <anweit00@hs-esslingen.de>
2022-03-10 13:42:25 +0100

    32 - redirect

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,8 +4,8 @@ import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 
-const Profile = (props) => {
 
+const Profile = (props) => {
 
     return (
         <div>
