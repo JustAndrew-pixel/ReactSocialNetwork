e1695253b5d83ca0eb043193961624920d00fad5
Andrej <anweit00@hs-esslingen.de>
2022-03-11 15:11:00 +0100

    35 - status: setState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../Common/Preloader/Preloader";
 import defaultUserPhoto from "../../../assets/images/user.png";
-
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
-  if(!props.profile){
-      return <Preloader/>
-  }
+    if (!props.profile) {
+        return <Preloader/>
+    }
 
 
     return (
         <div>
-        <div className={s.content}>
-            <img src='https://photocasa.ru/uploads/posts/2017-01/1485218882_img_5932.jpg'/>
-            </div>
-                <div className={s.descriptionBlock}>
-                    <img src={props.profile.photos.large != null ? props.profile.photos.large :defaultUserPhoto}/>
+            {/*<div className={s.content}>
+                <img src='https://photocasa.ru/uploads/posts/2017-01/1485218882_img_5932.jpg'/>
+            </div>*/}
+            <div className={s.descriptionBlock}>
+                <img src={props.profile.photos.large != null ? props.profile.photos.large : defaultUserPhoto}/>
                 ava+description
             </div>
-
+           <ProfileStatus status={'Hello everyone'}/>
         </div>
     );
 }
+
 
 export default ProfileInfo;
